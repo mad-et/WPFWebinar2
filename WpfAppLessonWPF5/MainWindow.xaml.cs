43b66af5d5dc271d2bea7c32ac27650ae465af24
Екатерина <mad-et@outlook.com>
2022-02-05 14:40:45 +0300

    Добавлены темы

--- WpfAppLessonWPF5/MainWindow.xaml.cs
+++ WpfAppLessonWPF5/MainWindow.xaml.cs
@@ -124,5 +124,13 @@ namespace WpfAppLessonWPF5
         {
             Application.Current.Shutdown();
         }
+
+        private void themes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Application.Current.Resources.MergedDictionaries.Clear();
+            Uri theme = new Uri(themes.SelectedIndex == 0 ? "Light.xaml" : "Dark.xaml",UriKind.Relative);
+            ResourceDictionary themeDict = Application.LoadComponent(theme) as ResourceDictionary;
+            Application.Current.Resources.MergedDictionaries.Add(themeDict);
+        }
     }
 }
